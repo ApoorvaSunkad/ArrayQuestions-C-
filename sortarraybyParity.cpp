d8f42be769b0eab77438fc4ed3911d216d216e5e
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-01-19 16:40:53 +0530

    Modified

--- sortarraybyParity.cpp
+++ sortarraybyParity.cpp
@@ -58,4 +58,29 @@ vector<int> sortArrayByParity(vector<int>& nums) {
         
         return v;
 }
+
+//Using two pointer approach
+//T.C = O(N)
+//S.C = O(1)
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> sortArrayByParity(vector<int>& nums) {
+    int sz = nums.size(), left = 0, right = sz-1;
+    while(left < right){
+            
+        while(left < right && nums[left] % 2 == 0){
+            left++;
+        }
+        while(right > left && nums[right] % 2 != 0){
+            right--;
+        }
+        if(left < right){
+            swap(nums[left], nums[right]);
+        }
+    }
+    return nums;
+}
     
