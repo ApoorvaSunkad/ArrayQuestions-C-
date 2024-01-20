b9a1e88be02f44a9e77ccfcac1fefdcabf145f10
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-01-20 13:31:52 +0530

    GFG

---
+++ nonrepeatingElement.cpp
@@ -0,0 +1,40 @@
+/*
+Find the first non-repeating element in a given array arr of N integers.
+Note: Array consists of only positive and negative integers and not zero.
+
+Example 1:
+
+Input : arr[] = {-1, 2, -1, 3, 2}
+Output : 3
+Explanation:
+-1 and 2 are repeating whereas 3 is 
+the only number occuring once.
+Hence, the output is 3. 
+ 
+
+Example 2:
+
+Input : arr[] = {1, 1, 1}
+Output : 0
+
+*/
+
+int firstNonRepeating(int arr[], int n) { 
+    // Complete the function
+    unordered_map<int,int> umap;
+        
+    for(int i = 0; i<n; i++){
+        umap[arr[i]]++;
+    }
+        
+    for(int i = 0; i<n; i++){
+        int key = arr[i];
+        auto temp = umap.find(key);
+            
+        if(temp->second == 1){
+            return key;
+        }
+    }
+        
+    return 0;
+} 
