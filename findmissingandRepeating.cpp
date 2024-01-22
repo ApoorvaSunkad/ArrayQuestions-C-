5e4606ed41284d040b1a862ddc827b13abe0bf4f
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-01-22 14:36:38 +0530

    GFG

--- findmissingandRepeating.cpp
+++ findmissingandRepeating.cpp
@@ -95,6 +95,69 @@ vector<int> findMissingRepeatingNumbers(vector<int> a) {
     }
     return {repeating, missing};
 
+}
+
+//Better approach 3:
+//Using vector
+
+//T.C = O(N)
+//S.C = O(N)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> findErrorNums(vector<int>& nums) {
+
+    int n =nums.size();
+
+    vector<int>v(n+1,0);
+
+    int missing=0,duplicate =0;
+
+    for(int i =0;i<n;i++){
+        v[nums[i]]++;
+    }
+
+    for(int i =1;i<v.size();i++){
+        if(v[i]==2)duplicate = i;
+        if(v[i]==0)missing = i;
+    }
+
+    return {duplicate,missing};
+}
+
+//Better approach: 4
+//Set + sum
+//T.C = O(N)
+//S.C = O(N)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> findErrorNums(vector<int>& nums) {
+
+    int n = nums.size();
+
+    int actual_sum = n * (n + 1) / 2;
+
+    int array_sum = 0, unique_sum = 0;
+
+    unordered_set<int> s(nums.begin(), nums.end());
+
+    for (int a : nums) {
+        array_sum += a;
+    }
+
+
+    for (int a : s) {
+        unique_sum += a;
+    }
+
+    int missing = actual_sum - unique_sum;
+    int duplicate = array_sum - unique_sum;
+
+    return {duplicate, missing};
+}
 
 //Optimised Approach:
 
