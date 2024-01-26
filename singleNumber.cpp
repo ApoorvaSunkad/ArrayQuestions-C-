44b2fcccce7043a7ee8a6e98e32acf965cce474b
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-01-26 15:10:03 +0530

    Modified single number

--- singleNumber.cpp
+++ singleNumber.cpp
@@ -10,6 +10,72 @@ Input: nums = [2,2,1]
 Output: 1
 
 */
+
+//Brute force method: Using maps
+#include<bits/stdc++.h>
+using namespace std;
+
+//T.C = O(N)
+//S.C = O(N)
+
+int singleNumber(vector<int>& nums) {
+
+    unordered_map<int,int> mp;
+
+    for(int i = 0; i<nums.size();i++){
+        mp[nums[i]]++;
+    }
+
+    for(auto it:mp){
+        if(it.second == 1){
+            return it.first;
+        }
+    }
+    return -1;
+}
+
+//Using sorting: Not constant space
+
+#include<bits/stdc++.h>
+using namespace std;
+//T.C = O(NLOGN)
+//S.C = O(1)
+int singleNumber(vector<int>& nums) { 
+
+    sort(nums.begin(),nums.end());
+
+    for(int i=1;i<nums.size();i+=2)
+    {
+        if(nums[i]!=nums[i-1])
+            return nums[i-1];
+    }
+    return nums[nums.size()-1];
+}
+
+//Using sets
+//T.C = O(N^2)
+//S.C = O(N)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int singleNumber(vector<int> &nums){
+    unordered_set<int>st;
+    for(int num:nums)
+    {
+        if(st.find(num)==st.end())
+        {
+            st.insert(num);
+        }
+        else
+        {
+            st.erase(num);
+        }
+    }
+    return *st.begin();
+}
+
+//Using XOR
 #include<bits/stdc++.h>
 using namespace std;
 int singleNumber(vector<int>& nums) {
