11e26f2f170acb12078b290c2b8ae0c99470487c
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-02-20 23:57:34 +0530

    Leetcode

--- MissingNumber.cpp
+++ MissingNumber.cpp
@@ -11,6 +11,33 @@ Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0
 
 */
 
+//Brute force 1:
+
+//Using map
+//T.C = O(2N)
+//S.C = O(N)
+
+int missingNumber(vector<int>& nums) {
+    int n = nums.size();
+
+    map<int,int> mp;
+
+    for(int i = 0; i<n; i++){
+        mp[nums[i]]++;
+    }
+
+    for(int i = 0; i<=n; i++){
+        int key = i;
+        if(mp.find(key)==mp.end()){
+            return key;
+        }
+    }
+    return 0;
+}
+
+//2nd brute force
+//T.C = O(N)
+//S.C = O(1)
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -23,4 +50,23 @@ int missingNumber(vector<int>& nums) {
         }
         int diff = sum-sum1;
         return diff;
+}
+
+
+//Optimised Solution: Using bit manipulation(XOR operator)
+//T.C = O(N)
+//S.C = O(1)
+
+int missingNumber(vector<int>& nums) {
+    int Xorr = 0;
+
+    for(int i = 0; i<=nums.size(); i++){
+        Xorr = Xorr^i;
+    }
+
+    for(int i = 0; i<nums.size(); i++){
+        Xorr = Xorr^nums[i];
+    }
+
+    return Xorr;
 }
